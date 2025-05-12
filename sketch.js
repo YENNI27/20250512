0f4b97602a0b63d07fec1a0d3c70614c839ca439
YENNI27 <juliahu27@gmail.com>
2025-05-12 10:23:42 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let facemesh;
+let video;
+let predictions = [];
+const points = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291
+,76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184];
+
+function setup() {
+  createCanvas(400, 400);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  facemesh = ml5.facemesh(video, modelReady);
+  facemesh.on('predict', gotResults);
+}
+
+function modelReady() {
+  // 模型載入完成
+}
+
+function gotResults(results) {
+  predictions = results;
+}
+
+function draw() {
+  background(220);
+  image(video, 0, 0, width, height);
+
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh;
+    stroke(255, 0, 0);
+    strokeWeight(5);
+    noFill();
+    beginShape();
+    for (let i = 0; i < points.length; i++) {
+      const idx = points[i];
+      const [x, y] = keypoints[idx];
+      vertex(x, y);
+    }
+    endShape();
+  }
+}
